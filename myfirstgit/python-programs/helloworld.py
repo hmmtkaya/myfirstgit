e67931759ccf643dbc130bbabc297a35b53e4564
hmmtkaya <145506134+hmmtkaya@users.noreply.github.com>
2023-09-21 20:42:41 +0100

    Update helloworld.py

--- myfirstgit/python-programs/helloworld.py
+++ myfirstgit/python-programs/helloworld.py
@@ -18,3 +18,12 @@ def calculator():
     secim = input("5-3")
     if secim == '1':
         sonuc = num1 + num2
+
+arket_listesi = ['vanilla tesco', 'cikolata tesco', 'sut tesco', 'seker tesco', 'sogan tesco', 'limon lidl', 'bira lidl', 'su lidl', 'soda tesco', 'ekmek lidl',
+                  'dondurma tesco', 'cips lidl', 'biber m&s', 'firin_kagidi lidl', 'poset lidl', 'pizza m&s', 'peynir lidl']
+
+liste_sayisi = 0
+for x in market_listesi:
+    liste_sayisi=liste_sayisi +1
+    esya, market = x.split()[0], x.split()[1]
+    print('{0}. esyanin adi {1} ve marketi {2}'.format(liste_sayisi, esya, market))
